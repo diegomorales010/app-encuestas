18e1116cf0a7dc2ba92f747bdeeb2b711bf7606e
Diego Morales <diemoret17@gmail.com>
2026-02-12 12:09:09 -0600

    padding ajustado para mobiles

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,7 +3,7 @@ import BottomNav from "../components/BottomNav";
 
 const MainLayout = () => {
   return (
-    <div className="min-h-screen bg-blue-50 pb-24">
+    <div className="min-h-screen bg-blue-50 pb-24 pt-6">
 
       {/* CONTENIDO VARIABLE */}
       <Outlet />
